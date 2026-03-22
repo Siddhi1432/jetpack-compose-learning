650a17a1a5d791d626f01256148cba7015b13ecd
Siddhi1432 <siddhirmane14@gmail.com>
2026-03-22 19:22:06 +0530

    Task 6: Profile card UI with image and card layout

--- app/src/main/java/com/siddhi/composelearning/MainActivity.kt
+++ app/src/main/java/com/siddhi/composelearning/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.*
 
 class MainActivity : ComponentActivity() {
@@ -15,26 +17,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var name by remember { mutableStateOf("")  }
-
             Column (
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                TextField(
-                    value = name,
-                    onValueChange = { name = it },
-                    label = { Text("Enter your name") },
-                )
+                Card (
+                    modifier = Modifier.fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.profile),
+                            contentDescription = "Profile Image",
+                            modifier = Modifier.size(100.dp)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
 
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Text(
-                    text = "Hello, $name!",
-                    fontSize = 22.sp
-                )
+                        Text(
+                            text = "Siddhi",
+                            fontSize = 22.sp
+                        )
+                        
+                        Text(
+                            text = "Android Developer",
+                            color = Color.Gray
+                        )
+                    }
+                }
             }
         }
     }
