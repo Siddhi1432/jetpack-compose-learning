5af5bcd322c5d42c46cd4676f3bbb63278ebf5cc
Siddhi1432 <siddhirmane14@gmail.com>
2026-03-22 20:12:45 +0530

    Task 7: LazyColumn list app with card UI

--- app/src/main/java/com/siddhi/composelearning/MainActivity.kt
+++ app/src/main/java/com/siddhi/composelearning/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material3.*
 import androidx.compose.ui.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.*
 
 class MainActivity : ComponentActivity() {
@@ -17,36 +15,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Card (
-                    modifier = Modifier.fillMaxWidth(),
-                    elevation = CardDefaults.cardElevation(8.dp)
-                ) {
-                    Column(
-                        modifier = Modifier.padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.profile),
-                            contentDescription = "Profile Image",
-                            modifier = Modifier.size(100.dp)
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
+            val names = listOf("Siddhi", "Rahul", "Priya", "Amit", "Neha")
 
+            LazyColumn (
+                modifier = Modifier.fillMaxSize()
+            ){
+                items(names){ name ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        elevation = CardDefaults.cardElevation(4.dp)
+                    ) {
                         Text(
-                            text = "Siddhi",
-                            fontSize = 22.sp
-                        )
-                        
-                        Text(
-                            text = "Android Developer",
-                            color = Color.Gray
+                            text = name,
+                            fontSize = 20.sp,
+                            modifier = Modifier.padding(16.dp)
                         )
                     }
                 }
