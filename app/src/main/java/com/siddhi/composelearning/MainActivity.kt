f898decf675bda2e1159f646d67bcfe0e83f34c6
Siddhi1432 <siddhirmane14@gmail.com>
2026-03-22 15:49:09 +0530

    Task 5: Text input app using TextField and state

--- app/src/main/java/com/siddhi/composelearning/MainActivity.kt
+++ app/src/main/java/com/siddhi/composelearning/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 
 class MainActivity : ComponentActivity() {
@@ -17,33 +15,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var count by remember { mutableStateOf(0)  }
+            var name by remember { mutableStateOf("")  }
 
             Column (
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "Count: $count",
-                    fontSize = 30.sp
+                TextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Enter your name") },
                 )
 
                 Spacer(modifier = Modifier.height(20.dp))
 
-                Row {
-                    Button(onClick = {count++}) {
-                        Text("+")
-                    }
-
-                    Spacer(modifier = Modifier.width(16.dp))
-
-                    Button(onClick = {count--}) {
-                        Text("-")
-                    }
-                }
+                Text(
+                    text = "Hello, $name!",
+                    fontSize = 22.sp
+                )
             }
         }
     }
