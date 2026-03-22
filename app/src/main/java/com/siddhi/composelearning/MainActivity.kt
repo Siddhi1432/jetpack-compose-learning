10786ffbb2a28883e8a4d1ca11ebfd303d6dd01d
Siddhi1432 <siddhirmane14@gmail.com>
2026-03-22 10:27:51 +0530

    Task 1: Setup Jetpack Compose project with basic UI

---
+++ app/src/main/java/com/siddhi/composelearning/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.siddhi.composelearning
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column {
+                Text("Hello Siddhi")
+                Text("Welcome to Jetpack Compose")
+            }
+        }
+    }
+}
